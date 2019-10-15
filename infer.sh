d917c1f76e56b7bae9bd2fca25d51c5b511988a8
jjeaby <32763196+jjeaby@users.noreply.github.com>
2019-10-15 19:24:45 +0900

    Update infer.sh

--- infer.sh
+++ infer.sh
@@ -1,1 +1,2 @@
-onmt-main --config lor.config --auto_config infer --features_file tok/src-valid.tok
+#onmt-main --config lor.config --auto_config infer --features_file tok/src-valid.tok
+onmt-main --config lor.config --auto_config infer --features_file test.tok  
