5379da5b12cc026e493ffa6f803a7f7eee961cb9
Lee YongJin <jjeaby@gmail.com>
2019-10-16 01:01:58 +0900

    src data modify

--- prepareSrc.py
+++ prepareSrc.py
@@ -7,12 +7,14 @@ srcFile = open("src.txt", mode="a", encoding="utf-8")
 dateFileLines = dateFile.readlines()
 weatherFileLines = weatherFile.readlines()
 
+count = 0
 for date in dateFileLines:
     tempDate = (date.split("	")[-1]).strip()
     for weather in weatherFileLines:
         if tempDate == weather.split(" ")[0].strip():
-            srcFile.write(weather.strip() + "\n")
-            print(weather)
+            count = count + 1
+            srcFile.write(str(count) + " " + weather.strip() + "\n")
+            print(str(count) + " " + weather.strip())
             break
 
 
