e3889892d24260018a146ae317bbe599277e3db0
jjeaby <32763196+jjeaby@users.noreply.github.com>
2019-10-15 19:24:02 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,3 @@
-CUDA_VISIBLE_DEVICES=0 TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_IGNORE_PERFORMANCE=1  onmt-main --model_type Transformer --auto_config --config lor.config  --mixed_precision train --num_gpu 1
+#CUDA_VISIBLE_DEVICES=0 TF_AUTO_MIXED_PRECISION_GRAPH_REWRITE_IGNORE_PERFORMANCE=1  onmt-main --model_type Transformer --auto_config --config lor.config  --mixed_precision train --num_gpu 1
+CUDA_VISIBLE_DEVICES=0,1,2,3 onmt-main --model_type Transformer --auto_config --config lor.config  --mixed_precision train --num_gpu 4
+#CUDA_VISIBLE_DEVICES=3 onmt-main --model_type Transformer --config lor.config --auto_config train --num_gpu 1
